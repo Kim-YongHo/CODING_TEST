cc20623f2b5735b2e7000975cb94bf3ad587e78a
Kim-YongHo <86473043+Kim-YongHo@users.noreply.github.com>
2021-07-03 17:02:45 +0900

    Greedy [이코테]

--- Greedy/03_Greedy_ex02.py
+++ Greedy/03_Greedy_ex02.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
   n, m, k = map(int, input().split())
   data = list(map(int, input().split()))
 
-  data.sort(reverse = True)
+  data.sort(reverse=True)
   first = data[0]
   second = data[1]
 
