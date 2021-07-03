fcd1baecdb1294e2ad83e76bb9f98a8bc66ed295
Kim-YongHo <86473043+Kim-YongHo@users.noreply.github.com>
2021-07-03 17:31:24 +0900

    Greedy [이코테]

--- Greedy/03_Greedy_ex02.py
+++ Greedy/03_Greedy_ex02.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
   first = data[0]
   second = data[1]
 
-  s_count = m//(k+1) ##최대값 횟수 일부, 2번째 값 횟수
+  s_count = m//(k+1)  ##최대값 횟수 일부, 2번째 값 횟수
   f_count = (s_count)*k + m%(k+1)
 
   total = (s_count*second) + (f_count*first)
