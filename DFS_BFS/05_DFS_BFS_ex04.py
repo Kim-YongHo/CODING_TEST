73a6a080479109ed52a6159083844dd2c7868c95
Kim-YongHo <86473043+Kim-YongHo@users.noreply.github.com>
2021-07-26 21:33:23 +0900

    DFS/BFS [이코테]

--- DFS_BFS/05_DFS_BFS_ex04.py
+++ DFS_BFS/05_DFS_BFS_ex04.py
@@ -1,7 +1,41 @@
 from collections import deque
 
-def bfs:
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if board[nx][ny] == 0:
+                continue
+
+            if board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
+                queue.append((nx, ny))
+
+
+    return board[n - 1][m - 1]
 
 
 if __name__ == '__main__':
-    n, m = in
+
+    n, m = map(int, input().split())
+    board = []
+
+    for i in range(n):
+        board.append(list(map(int, input())))
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    print(bfs(0, 0))
