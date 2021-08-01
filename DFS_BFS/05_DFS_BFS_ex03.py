107e2d1ee7f0e537417888c3fde7e00de3220eb3
Kim-YongHo <86473043+Kim-YongHo@users.noreply.github.com>
2021-08-01 22:37:00 +0900

    DFS BFS [이코테]

--- DFS_BFS/05_DFS_BFS_ex03.py
+++ DFS_BFS/05_DFS_BFS_ex03.py
@@ -1,8 +1,35 @@
 
+def dfs(x, y):
 
-if __nmae__ == '__main__':
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+
+    if graph[x][y] == 1:
+        return False
+
+    if graph[x][y] == 0:
+
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+
+        return True
+
+if __name__ == '__main__':
 
     n, m = map(int, input().split())
+
     graph = []
+    cnt = 0
+
     for i in range(n):
-        graph.append()
+        graph.append(list(map(int, input())))
+
+    for i in range(n):
+        for j in range(m):
+            if dfs(i, j) == True:
+                cnt += 1
+
+    print(cnt)
